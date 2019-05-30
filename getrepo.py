3193de17a4a93a28fed8967091e4dbbab1ada963
ahzz1207 <huangzhuangze@qq.com>
2019-05-30 15:53:56 +0800

    Initial commit

---
+++ getrepo.py
@@ -0,0 +1,35 @@
+import json
+import pymysql
+connect = pymysql.Connect(
+		host="localhost",
+		port=3306,
+		user="root",
+		passwd="17210240114",
+		db="githubreposfile",
+		charset='utf8'
+	)
+cursor = connect.cursor()
+sql = "insert into star20 values (%s, %s)"
+addrList = []
+
+with open("response.json") as f:
+    j = json.load(f)
+    for repo in j.get('data'):
+        local_addr = repo.get('local_addr')
+        addrList.append(local_addr)
+        repos_name = local_addr.split('/')[-2] + '$$%' + local_addr.split('/')[-1]
+        try:
+            cursor.excute(sql, (repo.get(id), repos_name))
+            connect.commit()
+        except:
+            connect.rollback()
+            print('faild')
+            
+        
+cursor.close()
+connect.close()
+
+with open('addrList.txt', 'w') as f:
+    for addr in addrList:
+        f.write(addr + '\n')
+
---
+++ h52txt.py
@@ -0,0 +1,38 @@
+import h5py
+import tables
+import pickle
+
+
+def load_vocab(vocab_file, sent):
+    vocab = {}
+    with open(vocab_file, 'rb') as f:
+        word = ""
+        info = pickle.load(f)  # dict
+        for word, index in dict.items(info):
+            vocab[index] = word
+        for num in sent:
+            word += vocab[num]
+            word += " "
+    return word
+
+
+def process(rfile, wfile, vfile):
+    table = tables.open_file(rfile)
+    data, index = (table.get_node('/phrases'), table.get_node('/indices'))
+
+    with open(wfile, "w+") as w:
+        for offset in range(index.shape[0]):
+            len, pos = index[offset]['length'], index[offset]['pos']
+            sent = data[pos: pos + len].astype('int32')
+            word = load_vocab(vfile, sent)
+            print(word)
+            w.write(word + '\n')
+
+
+feat = ["apiseq", "desc", "methname", "tokens"]
+
+for name in feat:
+    rfile = 'deepcs_dataset/train.' + name + '.h5'
+    wfile = 'deepcs_dataset/train.' + name + '.txt'
+    vfile = 'deepcs_dataset/vocab.' + name + '.pkl'
+    process(rfile, wfile, vfile)
